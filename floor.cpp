75518e905fd69739933793a1d0eaf5b81dcafdc5
Nika Zamani <nika.zamani@mths.ca>
2021-04-28 13:01:39 +0000

    added floor.cpp

---
+++ floor.cpp
@@ -0,0 +1,13 @@
+// Copyright (c) 2021 Nika Zamani All rights reserved 
+//
+// Created by Nika Zamani
+// Created on April 2021
+// This program uses libraries
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+    // this function returns the integer portion of a float 
+	std::cout << "The integer part of 12.4 is " << (floor(12.4)) << std::endl;
+}
