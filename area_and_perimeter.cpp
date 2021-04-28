0907eda25ba1dfc9ffd115ba76f9428258f7af86
Nika Zamani <nika.zamani@mths.ca>
2021-04-28 13:18:02 +0000

    added area_and_perimeter.cpp

---
+++ area_and_perimeter.cpp
@@ -0,0 +1,18 @@
+// Copyright (c) 2021 Nika Zamani All rights reserved
+//
+// Created by Nika Zamani
+// Created on April 2021
+// This program calculates that area and perimeter of a circle
+//    with raius 15mm
+
+#include <iostream>
+#include <cmath>
+
+int main() {
+std::cout << "If a circle has a radius of 15mm: " << std::endl;
+std::cout << std::endl;
+std::cout << "Area is " << (M_PI * pow(15, 2)) << "mm²." << std::endl;
+std::cout << "Perimeter is " << (2 * M_PI * 15) << "mm." << std::endl;
+std::cout << std::endl;
+std::cout << "Done." << std::endl;
+}
